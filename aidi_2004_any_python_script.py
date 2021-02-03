8e627adb6e2ef8b8b1dd3cd58aaa8e6648e34edc
sagunesh.grover <saguneshgrover@gmail.com>
2021-02-03 07:56:27 +0530

    Any Python Script Added

---
+++ aidi_2004_any_python_script.py
@@ -0,0 +1,8 @@
+#script to find whether a number is odd or even
+
+
+num = int(input("Please enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
